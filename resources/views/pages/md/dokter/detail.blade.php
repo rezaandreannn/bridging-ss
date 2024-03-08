0bd1da61fa4528a54cd7ef12ceb9d141ca006f84
dimasbudipratama <dimasbudipratama007.com>
2024-03-08 09:28:45 +0700

    detail dokter

--- resources/views/pages/md/dokter/detail.blade.php
+++ resources/views/pages/md/dokter/detail.blade.php
@@ -12,10 +12,10 @@
 <div class="main-content">
     <section class="section">
         <div class="section-header">
-            <h1>Detail Pasien</h1>
+            <h1>Detail Practitioner</h1>
             <div class="section-header-breadcrumb">
-                <div class="breadcrumb-item active"><a href="#">Master dokter</a></div>
-                <div class="breadcrumb-item"><a href="#">Organization</a></div>
+                <div class="breadcrumb-item active"><a href="{{ route('dokter.index') }}">Master dokter</a></div>
+                <div class="breadcrumb-item"><a href="{{ route('dokter.index') }}">Practitioner</a></div>
                 <div class="breadcrumb-item">Detail Organization</div>
             </div>
         </div>
@@ -23,76 +23,75 @@
         <div class="section-body">
             <div class="row">
                 <div class="col-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4>Show/Hide</h4>
-                            <div class="card-header-action">
-                                <a dokter-collapse="#mycard-collapse" class="btn btn-icon btn-info" href="#"><i class="fas fa-minus"></i></a>
+                    <div class="card author-box card-primary">
+                        <div class="card-body">
+                            <div class="author-box-name">
+                                <a href="#">{{ $dokter['data']['nama_dokter'] ?? ''}} - ( {{ $dokter['data']['kode_dokter'] ?? ''}} )</a>
                             </div>
-                        </div>
-                        <div class="collapse show" id="mycard-collapse">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-12 col-md-6">
-                                        <div class="row">
-                                            <div class="col-md-4">
-                                                Pasien ID
-                                            </div>
-                                            <div class="col-md-8">
-                                                : {{ $practitionerSatuSehat['kode_dokter'] ?? ''}}
-                                            </div>
-                                            <div class="col-md-4">
-                                                Name
-                                            </div>
-                                            <div class="col-md-8">
-                                                : {{ $dokter['jenis_profesi'] ?? ''}}
-                                                
-                                            </div>
-                                            <div class="col-md-4">
-                                                BPJS
-                                            </div>
-                                            <div class="col-md-8">
-                                                : {{ $dokter['spesialis'] ?? ''}}
-                                            </div>
-                                            <div class="col-md-4">
-                                                NIK
-                                            </div>
-                                            <div class="col-md-8">
-                                                : {{ $dokter['nama_dokter'] ?? ''}}
+                            <div class="author-box-job"><b>{{ $dokter['data']['jenis_profesi'] ?? ''}}</b> ({{ $dokter['data']['spesialis'] ?? ''}})</div>
+                                        <div class="author-box-description">
+                                            <div class="row">
+                                                <div class="col-12 col-md-6">
+                                                    <div class="row">
+                                                        <div class="col-md-4">
+                                                            NIK
+                                                        </div>
+                                                        <div class="col-md-8">
+                                                            : {{ $dokter['data']['nik'] ?? ''}}
+                                                        </div>
+                                                        <div class="col-md-4">
+                                                            Email
+                                                        </div>
+                                                        <div class="col-md-8">
+                                                            : {{ $dokter['data']['email'] ?? ''}}
+                                                        
+                                                        </div>
+                                                        <div class="col-md-4">
+                                                            Tanggal Lahir
+                                                        </div>
+                                                        <div class="col-md-8">
+                                                            : {{ date('d-m-Y', strtotime($dokter['data']['tgl_lahir'] ?? ''))  }}
+                                                        </div>
+                                                        <div class="col-md-4">
+                                                            Agama
+                                                        </div>
+                                                        <div class="col-md-8">
+                                                            : {{ $dokter['data']['agama'] ?? ''}}
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-12 col-md-6">
+                                                    <div class="row">
+                                                        <div class="col-md-4">
+                                                            Provinsi
+                                                        </div>
+                                                        <div class="col-md-8">
+                                                            : {{ $dokter['data']['provinsi'] ?? ''}}
+                                                        
+                                                        </div>
+                                                        <div class="col-md-4">
+                                                            Kota
+                                                        </div>
+                                                        <div class="col-md-8">
+                                                            : {{ $dokter['data']['kota'] ?? ''}}
+                                                        </div>
+                                                        <div class="col-md-4">
+                                                            Kode Pos
+                                                        </div>
+                                                        <div class="col-md-8">
+                                                            : {{ $dokter['data']['kode_pos'] ?? ''}}
+                                                        </div>
+                                                        <div class="col-md-4">
+                                                            Alamat
+                                                        </div>
+                                                        <div class="col-md-8">
+                                                            : {{ $dokter['data']['alamat'] ?? ''}}
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="col-12 col-md-6">
-                                        <div class="row">
-                                            <div class="col-md-4">
-                                                No HP
-                                            </div>
-                                            <div class="col-md-8">
-                                                : {{ $dokter['nik'] ?? ''}}
-                                            </div>
-                                            <div class="col-md-4">
-                                                Tanggal Lahir
-                                            </div>
-                                            <div class="col-md-8">
-                                                : {{ $dokter['email'] ?? ''}}
-                                            
-                                            </div>
-                                            <div class="col-md-4">
-                                                Jenis Kelamin
-                                            </div>
-                                            <div class="col-md-8">
-                                                : {{ $dokter['agama'] ?? ''}}
-                                            </div>
-                                            <div class="col-md-4">
-                                                Postal code
-                                            </div>
-                                            <div class="col-md-8">
-                                                : {{ $dokter['no_hp'] ?? ''}}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            <div class="w-100 d-sm-none"></div>
                         </div>
                     </div>
                 </div>
