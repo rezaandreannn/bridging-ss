4a0799ed54fbf2aa8ff7aa9731c3c03bd2c93359
rezaandreannn <andreanreza042@gmail.com>
2024-03-11 21:08:59 +0700

    fix Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,16 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        Organization::create([
-            'organization_id' => '861b3be1-9a10-4cc1-b34d-8d5d23779d54',
-            'name' => 'STAGING itrsumm08@gmail.com',
-            'created_by' => 'seeder'
-        ]);
+        $this->call(UserTableSeeder::class);
+        $this->call(OrganizationSeeder::class);
+        $this->call(LocationTableSeeder::class);
     }
 }
