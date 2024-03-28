70201b7a9a15ba9a03b85d5968c1f30b0a7b45e7
dimasbudipratama <dimasbudipratama007.com>
2024-03-28 08:15:05 +0700

    foto

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -7,7 +7,7 @@
     </form>
     <ul class="navbar-nav navbar-right">
         <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-                <img alt="image" src="{{ asset('img/avatar/avatar-1.png') }}" class="rounded-circle mr-1">
+                <img alt="image" src="{{ asset('storage/images/'. Auth::user()->image ) }}" class="rounded-circle mr-1">
                 <div class="d-sm-none d-lg-inline-block">{{ Auth::user()->name }}</div>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
