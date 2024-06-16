fb85fbf7b019e794f7095aaa8685b46ea49d1207
Myusuf123 <yusufmuhammad690@gmail.com>
2024-06-16 09:07:20 +0700

    table contoh asesmen dokter

---
+++ database/migrations/2024_06_16_083845_create_assesmen_dokter_fisioterapi_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('pku')->create('assesmen_dokter_fisioterapi', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('pku')->dropIfExists('assesmen_dokter_fisioterapi');
+    }
+};
