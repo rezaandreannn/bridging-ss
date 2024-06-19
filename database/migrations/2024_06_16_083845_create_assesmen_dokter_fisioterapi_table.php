1945b5f6fc0f18f3d4e0298eac8b28bb4c233c24
itrsumm <itrsumm08@gmail.com>
2024-06-19 15:50:55 +0700

    fisioterapi migration

--- database/migrations/2024_06_16_083845_create_assesmen_dokter_fisioterapi_table.php
+++ database/migrations/2024_06_16_083845_create_assesmen_dokter_fisioterapi_table.php
@@ -15,7 +15,33 @@ return new class extends Migration
     {
         Schema::connection('pku')->create('assesmen_dokter_fisioterapi', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('no_registrasi');
+            $table->string('kode_transaksi_fisio');
+            $table->date('tanggal');
+            $table->time('jam');
+            $table->string('cara_datang');
+            $table->string('deskripsi_cara_datang');
+            $table->text('anamnesa');
+            $table->string('keadaan_umum');
+            $table->string('kesadaran');
+            $table->string('tekanan_darah');
+            $table->string('nadi');
+            $table->string('respirasi');
+            $table->string('suhu');
+            $table->string('berat_badan');
+            $table->string('prothesa');
+            $table->string('orthosis');
+            $table->string('status_psikologi');
+            $table->string('status_mental');
+            $table->string('diagnosa_klinis');
+            $table->string('rencana_tindakan');
+            $table->text('jenis_tindakan')->nullable();
+            $table->string('rencana_rujukan');
+            $table->text('deskripsi_rujukan')->nullable();
+            $table->string('rencana_konsul');
+            $table->text('deskripsi_konsul')->nullable();
+            $table->integer('anjuran_terapi');
+            $table->integer('evaluasi_terapi');
             $table->timestamps();
         });
     }
