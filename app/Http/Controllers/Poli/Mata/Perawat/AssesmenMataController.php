ee954db5170938664a95e3c9997d6873f0137bc7
itrsumm <itrsumm08@gmail.com>
2024-10-10 08:37:57 +0700

    dd reesep

--- app/Http/Controllers/Poli/Mata/Perawat/AssesmenMataController.php
+++ app/Http/Controllers/Poli/Mata/Perawat/AssesmenMataController.php
@@ -176,7 +176,7 @@ class AssesmenMataController extends Controller
     public function cetakResep($noReg, $kode_transaksi)
     {
         $data = $this->poliMata->cetakResep($noReg, $kode_transaksi);
-        dd($data);
+        // dd($data);
         $biodata = $this->rekam_medis->getBiodata($noReg);
         $antrian = $this->rekam_medis->getAntrianObat($kode_transaksi);
         // dd($antrian);
