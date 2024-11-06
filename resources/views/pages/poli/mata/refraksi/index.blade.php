91de81d592c9a104916c1eb6e2d66eb9a73284ca
dimasbudipratama <dimasbudipratama007.com>
2024-11-06 10:50:26 +0700

    refraksi mata

--- resources/views/pages/poli/mata/refraksi/index.blade.php
+++ resources/views/pages/poli/mata/refraksi/index.blade.php
@@ -107,6 +107,50 @@
                     </div>
                 </div>
             </div>
+            <div class="card">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table-striped table" id="table-1">
+                            <thead>
+                                <tr>
+                                    <th scope="col">No MR</th>
+                                    <th scope="col">Nama Pasien</th>
+                                    <th scope="col">Alamat</th>
+                                    <th scope="col">Dokter Pengirim</th>
+                                    <th scope="col">Tanggal</th>
+                                    <th scope="col">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($pasienKonsul as $konsul)
+                                <tr>
+                                    <td>{{$konsul->NO_MR}}</td>
+                                    <td>{{$konsul->NAMA_PASIEN}}</td>
+                                    <td>{{$konsul->ALAMAT}}</td>
+                                    <td>{{$konsul->NAMA_DOKTER}}</td>
+                                    <td>{{$konsul->TANGGAL}}</td>
+                                    <td>
+                                
+                                        @if($data->FS_STATUS == 1)
+                                        @if($poliMata->cekRefraksi($data->No_Reg) == true)
+                                        <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#modal-edit{{$konsul->NO_REG}}">
+                                            <i class="fas fa-plus"></i> Edit
+                                        </button>
+                                        @else
+                                        <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#modal-add-refraksi2{{$konsul->NO_REG}}">
+                                            <i class="fas fa-plus"></i> Entry
+                                        </button>
+                                        @endif
+                                    @endif
+                                    </td>
+                                </tr>
+                                @endforeach
+                               
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </div>
