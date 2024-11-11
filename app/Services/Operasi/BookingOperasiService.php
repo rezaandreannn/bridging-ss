c9b3d1c1da897f51c22af5be7a87d3dd41dd31ab
rezaandreannn <andreanreza042@gmail.com>
2024-11-11 14:39:01 +0700

    service booking

---
+++ app/Services/Operasi/BookingOperasiService.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Services\Operasi;
+
+use App\Models\Operasi\BookingOperasi;
+
+class BookingOperasiService
+{
+    public function get()
+    {
+        return BookingOperasi::with([
+            'pendaftaran' => function ($query) {
+                $query->select('No_Reg', 'No_MR')
+                    ->with(['registerPasien' => function ($query) {
+                        $query->select('No_MR', 'Nama_Pasien');
+                    }]);
+            },
+            'ruangan' => function ($query) {
+                $query->select('id', 'kode_ruang', 'nama_ruang');
+            }
+        ])->get();
+    }
+
+    public function byDate($date)
+    {
+        return BookingOperasi::with([
+            'pendaftaran' => function ($query) {
+                $query->select('No_Reg', 'No_MR')
+                    ->with(['registerPasien' => function ($query) {
+                        $query->select('No_MR', 'Nama_Pasien');
+                    }]);
+            },
+            'ruangan' => function ($query) {
+                $query->select('id', 'kode_ruang', 'nama_ruang');
+            }
+        ])
+            ->whereDate('tanggal', $date)
+            ->get();
+    }
+
+    public function byRegister($kodeRegister)
+    {
+        return BookingOperasi::with([
+            'pendaftaran' => function ($query) {
+                $query->select('No_Reg', 'No_MR')
+                    ->with(['registerPasien' => function ($query) {
+                        $query->select('No_MR', 'Nama_Pasien');
+                    }]);
+            },
+            'ruangan' => function ($query) {
+                $query->select('id', 'kode_ruang', 'nama_ruang');
+            }
+        ])
+            ->where('kode_register', $kodeRegister)
+            ->get();
+    }
+}
