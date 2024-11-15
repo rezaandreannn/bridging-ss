97223b5844bf186fb4d4c6622e46c4ef64a73006
rezaandreannn <andreanreza042@gmail.com>
2024-11-15 15:12:53 +0700

    penandaan operasi service

--- app/Services/Operasi/PenandaanOperasiService.php
+++ app/Services/Operasi/PenandaanOperasiService.php
@@ -2,21 +2,45 @@
 
 namespace App\Services\Operasi;
 
-use Exception;
-use Illuminate\Support\Facades\DB;
+
 use Illuminate\Support\Facades\Storage;
 use App\Models\Operasi\PenandaanOperasi;
 
 class PenandaanOperasiService
 {
+    private function baseQuery()
+    {
+        return PenandaanOperasi::with([
+            'booking' => function ($query) {
+                $query->select('kode_register', 'tanggal', 'ruangan_id', 'kode_dokter', 'nama_tindakan')
+                    ->with([
+                        'pendaftaran' => function ($query) {
+                            $query->select('No_Reg', 'No_MR')
+                                ->with(['registerPasien' => function ($query) {
+                                    $query->select('No_MR', 'Nama_Pasien');
+                                }]);
+                        },
+                        'ruangan' => function ($query) {
+                            $query->select('id', 'kode_ruang', 'nama_ruang');
+                        },
+                        'dokter' => function ($query) {
+                            $query->select('Kode_Dokter', 'Nama_Dokter');
+                        }
+                    ]);
+            }
+        ]);
+    }
+
     public function get()
     {
-        return PenandaanOperasi::all();
+        $penandaans = $this->baseQuery()->get();
+        return $this->mapData($penandaans);
     }
 
     public function findById($id)
     {
-        return PenandaanOperasi::find($id);
+        $penandaan = PenandaanOperasi::find($id);
+        return $this->mapData($penandaan);
     }
 
 
@@ -53,4 +77,21 @@ class PenandaanOperasiService
     }
     public function update($id, array $data) {}
     public function delete($id) {}
+
+    private function mapData($penandaans)
+    {
+        return collect($penandaans->map(function ($item) {
+            return (object) [
+                'id' => $item->id,
+                'kode_register' => $item->kode_register,
+                'tanggal' => optional($item->booking)->tanggal,
+                'gambar' => $item->hasil_gambar,
+                'no_mr' => optional($item->booking->pendaftaran)->No_MR,
+                'nama_pasien' => optional($item->booking->pendaftaran->registerPasien)->Nama_Pasien,
+                'ruang_operasi' => optional($item->booking->ruangan)->nama_ruang,
+                'nama_dokter' => optional($item->booking->dokter)->Nama_Dokter,
+                'nama_tindakan' => optional($item->booking)->nama_tindakan
+            ];
+        }));
+    }
 }
