9d025f97441a7d727415ed20b083babd48d0cf1f
dimasbudipratama <dimasbudipratama007.com>
2024-11-15 10:55:48 +0700

    penandaan operasi

--- app/Services/Operasi/PenandaanOperasiService.php
+++ app/Services/Operasi/PenandaanOperasiService.php
@@ -10,11 +10,12 @@ class PenandaanOperasiService
 {
     public function get()
     {
-        $databookings = $this->baseQuery()->get();
-        return $this->mapData($databookings);
+        return PenandaanOperasi::all();
     }
+
     public function byRegister($kodeRegister) {}
     public function byRegisterWithTtd($kodeRegister) {}
+
     public function insert(array $data) {}
     public function update($id, array $data) {}
     public function delete($id) {}
