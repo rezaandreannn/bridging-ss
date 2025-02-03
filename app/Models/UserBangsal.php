591d6732059f3744e95824f93f9291da918de62c
yusufmuhammad690@gmail.com <itrsumm08@gmail.com>
2025-02-03 13:21:21 +0700

    manage user pengguna

--- app/Models/UserBangsal.php
+++ app/Models/UserBangsal.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\MasterData\Bangsal;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UserBangsal extends Model
 {
@@ -11,10 +12,17 @@ class UserBangsal extends Model
     protected $connection = 'sqlsrv';
     protected $table = 'user_bangsals';
 
+    protected $with = ['bangsal'];
+
     protected $fillable = [
         'user_id',
         'kode_bangsal',
         'created_at',
         'updated_at'
     ];
+
+    public function bangsal()
+    {
+        return $this->belongsTo(Bangsal::class, 'kode_bangsal', 'Kode_Bangsal');
+    }
 }
